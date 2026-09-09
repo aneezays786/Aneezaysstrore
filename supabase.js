cf757af1886390250acfd0e86905d1b77a44469b
Amna Nadeem <aamnanadeem619@gmail.com>
2026-09-09 16:08:34 +0500

    Delete supabase.js

--- supabase.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
-const SUPABASE_URL='YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY='YOUR_SUPABASE_ANON_KEY';
-export const supabase=(SUPABASE_URL.startsWith('http') && SUPABASE_ANON_KEY!=='YOUR_SUPABASE_ANON_KEY')?createClient(SUPABASE_URL,SUPABASE_ANON_KEY):null;
